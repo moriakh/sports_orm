bd0c6f9f72081aeb01d75c7d1b95bd62ec398fd8
Moriak Nunez <moriaknc@outlook.com>
2021-08-14 10:35:43 -0400

    prueba de sonido

--- leagues/urls.py
+++ leagues/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name="index"),
 	path('task', views.task, name="task"),
+	path('task2', views.task2, name="task2"),
 	path('initialize', views.make_data, name="make_data"),
 ]
--- leagues/views.py
+++ leagues/views.py
@@ -38,3 +38,36 @@ def task(request):
 		'names' : Player.objects.filter(first_name = "Alexander") | Player.objects.filter(first_name = "Wyatt")
 	}
 	return render(request, "task.html", context)
+
+def task2(request):
+	try:
+		atlantic_teams = League.objects.get(name = "Atlantic Soccer Conference").teams.all()
+	except League.DoesNotExist:
+		atlantic_teams = []
+
+	try:
+		loswichitavikin = Team.objects.get(team_name = "Vikings", location = "Wichita")
+		wichita_players = loswichitavikin.all_players.all()
+		wichita_current_ids = [player.id for player in loswichitavikin.curr_players.all()]
+		not_now_wichita = [player for player in wichita_players if player.id not in wichita_current_ids]
+
+	except Team.DoesNotExist:
+		not_now_wichita = []
+
+	# Player.objects.filter(all_teams__team_name='Vikings', all_teams__location='Wichita').exclude(curr_team__team_name='Vikings', curr_team__location='Wichita')
+
+	context = {
+		'atlantic_teams' : atlantic_teams,
+		'boston_penguins' : Player.objects.filter(curr_team__team_name='Boston Penguins'),
+		'international_collegiate' : Player.objects.filter(curr_team__league__name="International Collegiate Baseball Conference"),
+		'lopez' : Player.objects.filter(curr_team__league__name="American Association of Amateur Soccer").filter(last_name="Lopez"),
+		'football_players' : Player.objects.filter(curr_team__league__sport="Football"),
+		'team_sophia' : Team.objects.filter(all_players__first_name="Sophia"),
+		'league_sophia' : League.objects.filter(teams__all_players__first_name="Sophia"),
+		'flores' : Player.objects.filter(last_name="Flores").exclude(curr_team__team_name="Roughriders", curr_team__location="Washington"),
+		'samuel_evans' : Team.objects.filter(all_players__first_name="Samuel", all_players__last_name="Evans"),
+		'not_now_wichita' : not_now_wichita,
+		
+
+	}
+	return render(request, "task2.html", context)
